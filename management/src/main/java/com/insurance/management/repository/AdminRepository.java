f02553c8ed9f75f74f612c6f2b214ff05670550e
kaustubh54 <68216145+kaustubh54@users.noreply.github.com>
2022-03-30 17:08:15 +0530

    Delete management directory

--- management/src/main/java/com/insurance/management/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insurance.management.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import com.insurance.management.model.Admin;
-public interface AdminRepository extends JpaRepository<Admin, String> {
-	@Query("SELECT a FROM Admin a WHERE a.username =?1 and a.password=?2")
-	public  Admin validateAdmin(String username,String password);
-	}
-
